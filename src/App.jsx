042aecc81d8f34aba058859ca9bce0c5654b26a4
MykhailoLytvyn <cresedel211194@gmail.com>
2023-09-30 00:49:00 +0200

    telegram sdk

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
+import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
+import WebApp from '@twa-dev/sdk'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,13 +23,13 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        {/* Here we add our button with alert callback */}
+      <div className="card">
+        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
+            Show Alert
+        </button>
+      </div>
     </>
   )
 }
