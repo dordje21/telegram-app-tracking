b934e7c9e2a0668710c8af5123585114e1a87d03
MykhailoLytvyn <cresedel211194@gmail.com>
2023-09-30 02:07:30 +0200

    breanchs

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   build: {
     outDir: './docs'
   },
-  base: '/my-react-telegram-web-app/'
+  base: '/'
 })
