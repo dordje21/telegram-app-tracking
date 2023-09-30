4615506b2e5996cbaabe3ca7c618bc582327e5d7
MykhailoLytvyn <cresedel211194@gmail.com>
2023-09-30 02:04:14 +0200

    breanchs

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   build: {
     outDir: './docs'
   },
